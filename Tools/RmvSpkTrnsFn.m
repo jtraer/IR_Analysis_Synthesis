32871f1303ef0297e5b41b349ac53b77c685a5ae
jtraer <jtraer@mit.edu>
2017-07-31 20:00:29 -0400

    fixing bug

--- Tools/RmvSpkTrnsFn.m
+++ Tools/RmvSpkTrnsFn.m
@@ -12,7 +12,7 @@ Nf(find(Nf==0))=[]; % remove the omnidirectional IR
 cff=C(ndx(1)).Spcff;
 cSpc=zeros(Nf,1);
 for jc=1:length(C);
-    spc=interp1([0 C(jc).Spcff C(jc).fs],[0; C(jc).spc; 0],cff);
+    spc=interp1([0; C(jc).Spcff(:); C(jc).fs],[0; C(jc).spc(:); 0],cff(:));
     C(jc).Path
     size(cSpc)
     size(spc)
@@ -34,7 +34,7 @@ nft=2^max(ceil(log2([length(tmp) 2*length(cSpc)])));
 tff=[1:nft]/nft*fs;
 NH=fft(h,nft);
 NH=NH(1:nft/2);
-T=interp1([0 cff tff(end)],[cSpc(1); cSpc; cSpc(end) ],tff(1:nft/2));
+T=interp1([0; cff(:); tff(end)],[cSpc(1); cSpc(:); cSpc(end) ],tff(1:nft/2));
 fNH=NH./abs(T(:))*mean(abs(T));
 h_cal=ifft([fNH; 0; flipud(conj(fNH(2:end)))]);
 h_cal=h_cal(1:Npd);

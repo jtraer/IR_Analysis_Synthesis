f2baea24a353ce6c0de9f9bb0a82985f9d0e6524
jtraer <jtraer@mit.edu>
2017-08-15 17:27:09 -0400

    adding a new plot to show modes in teh IR spectrum

---
+++ PltIRMdSpc.m
@@ -0,0 +1,19 @@
+function PltIRMdSpc(H,C)
+
+% plot the spectrum
+nft=2^ceil(log2(length(H.h)));
+spc=fft(H.h,nft);
+spc=spc(1:end/2);
+Spcff=[1:nft/2]*H.fs/nft;
+
+plot(10*log10(abs(spc)),Spcff/1e3);
+hold on;
+for jm=1:length(H.Modes)
+    [~,ndx]=min(abs(Spcff-H.Modes(jm).cf));
+    plot(10*log10([min(abs(spc) abs(spc(ndx)))]),Spcff(ndx)*ones(1,2),'r-');
+end
+
+xlabel('Power (db)');
+ylabel('Frequency (kHz)')
+set(gca,'yscale','log')
+title([H.Path ': Power Spectrum and identified modes'])

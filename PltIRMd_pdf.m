cc90688afe3e48ddcf79304588847106131c3ea3
jtraer <jtraer@mit.edu>
2017-09-10 20:34:09 -0400

    adding new plot

--- PltIRMd_pdf.m
+++ PltIRMd_pdf.m
@@ -3,6 +3,8 @@ function PltIRMd_pdf(H,C)
 ff=linspace(H.ff(1),H.ff(end),100);
 Rtt=linspace(0,3*max([H.Modes.RT60]),100);
 Opp=linspace(min([H.Modes.OnPwr])-20,max([H.Modes.OnPwr])+20,100);
+Rtt=linspace(0,3*max([H.Modes.RT60]),100);
+Opp=linspace(min([H.Modes.OnPwr])-20,max([H.Modes.OnPwr])+20,100);
 
 subplot(1,3,1);
 pdf=normpdf(ff,H.MdStts.pdf_f.mu,H.MdStts.pdf_f.sigma);
@@ -24,10 +26,9 @@ hp=plot(Opp,pdf,'-');
 pclr=get(hp,'color');
 set(hp,'linewidth',3);
 hold on;
-set(gca,'xscale','log');
-hold on
-hold off;
 axis tight;
+set(gca,'xscale','log');
+set(gca,'xlim',[1e-2 3]);
 xlabel('Mode RT60 (s)');
 ylabel('Gaussian fit')
 title([H.Path ': Modes '])
@@ -37,8 +38,7 @@ pdf=normpdf(Opp,H.MdStts.pdf_OP.mu,H.MdStts.pdf_OP.sigma);
 hp=plot(Opp,pdf,'-');
 pclr=get(hp,'color');
 set(hp,'linewidth',3);
-hold on
-hold off;
 axis tight;
+set(gca,'xlim',[-80 40]);
 xlabel('Mode Onset Power (db)');
 ylabel('Gaussian fit')
